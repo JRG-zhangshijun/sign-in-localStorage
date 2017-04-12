3677d9ca5a1c19a7638aa8f13ae30b735d291d96
JRG-zhangshijun <462895506@qq.com>
2017-04-12 21:33:58 +0800

    使用webpack-dev-server

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ function component () {
   var element = j('<div></div>');
   /* lodash is required for the next line to work */
   //element.innerHTML = _.join(['Hello','webpack'], ' ');
-  element.html(_.join(['Hello','webpack'], ' '))
+  element.html(_.join(['Hi','webpack','Good Job','Good idea'], ' '))
   //return element;
   return element.get(0);
 }
