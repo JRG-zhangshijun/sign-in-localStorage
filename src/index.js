066c1d85ffaa9d08b35bc1b60dc80e960899de49
JRG-zhangshijun <462895506@qq.com>
2017-04-13 23:50:43 +0800

    new

--- src/index.js
+++ src/index.js
@@ -7,16 +7,3 @@ ReactDOM.render(
   <h1>Don't work overtime!</h1>,
   document.getElementById('root2')
 );
-/*function tick(){
-  const element = (
-    <div>
-      <h1>Hello,world</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
-  ReactDOM.render(
-    element,
-    document.getElementById('root2')
-  )
-}
-setInterval(tick,1000)*/
