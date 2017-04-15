cddc926491436c0f55df77bd45175d795c38f3c7
JRG-zhangshijun <462895506@qq.com>
2017-04-16 01:01:09 +0800

    todolist-pre

--- src/index.js
+++ src/index.js
@@ -2,8 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+//import Welcome from './Welcome'
+/*function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root2')
+  );
+}
 
+setInterval(tick, 1000);*/
+// class Welcome extends React.Component { 
+//   render() {
+//     return <h1>Hello, Component</h1>;
+//   }
+// }
 ReactDOM.render(
-  <h1>No work overtime!</h1>,
+  //<Welcome/>,
+  <Welcome name="frank"/>,
   document.getElementById('root2')
-);
+)
